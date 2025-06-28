2fc54230f9d25f77bb12e930ca1938f61fc38a64
Priyanshu Jindal <priyanshujindal2006@gmail.com>
2025-06-28 17:49:33 +0530

    Fix Node.js 16.x compatibility: Downgrade to ESLint 8.x and use .eslintrc.js format

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    node: true,
+    es2020: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module'
+  },
+  rules: {
+    'indent': ['error', 2],
+    'linebreak-style': 'off',
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+    'no-unused-vars': ['warn'],
+    'no-console': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error'
+  },
+  ignorePatterns: [
+    'node_modules/',
+    '.npm-cache/',
+    '*.min.js'
+  ]
+}; 
